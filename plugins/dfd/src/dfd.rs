b4b92d8c44c516427d4a36cf2a9f89049ec08e89
Joao-Felisberto <joaofelisberto@ua.pt>
2024-07-05 21:30:15 +0100

    Basic dfd plugin

--- plugins/dfd/src/dfd.rs
+++ plugins/dfd/src/dfd.rs
@@ -1,15 +1,18 @@
 use serde::{Serialize,Deserialize};
+use serde_yml::Value;
+use core::panic;
+use std::collections::HashMap;
 
 #[derive(Serialize,Deserialize)]
 pub struct Node {
-    id: usize,
+    id: String,
     label: String,
 }
 
 #[derive(Serialize,Deserialize)]
 pub struct Edge {
-    from: usize,
-    to: usize,
+    from: String,
+    to: String,
 }
 
 #[derive(Serialize,Deserialize)]
@@ -19,17 +22,32 @@ pub struct Data {
 }
 
 impl Node {
-    pub fn new(id: usize, label: String) -> Self {
-        Node {id, label}
+    pub fn new(id: String, label: String) -> Self {
+        let mut id = id;
+        if id.starts_with(":") {
+            id = id[1..].to_string()
+        }
+
+        Node { id, label, }
     }
 
-    pub fn get_id(&self) -> usize {
-        self.id
+    pub fn get_id(&self) -> String {
+        self.id.clone()
     }
 }
 
 impl Edge {
-    pub fn new(from: usize, to: usize) -> Self {
+    pub fn new(from: String, to: String) -> Self {
+        let mut from = from;
+        if from.starts_with(":") {
+            from = from[1..].to_string()
+        }
+        
+        let mut to = to;
+        if to.starts_with(":") {
+            to = to[1..].to_string()
+        }
+
         Edge {from, to}
     }
 }
@@ -37,6 +55,80 @@ impl Edge {
 impl Data {
     pub fn new(nodes: Vec<Node>, edges: Vec<Edge>) -> Self {
         Data {nodes, edges}
+    }
+
+    pub fn from_yaml(yaml_str: &str) -> Data {
+        let mut nodes = vec![];
+        let mut edges = vec![];
+
+        let yaml: HashMap<String, Value> = serde_yml::from_str(yaml_str).expect("Could not deserialize yaml");
+
+        let data_flows = &yaml["data flows"];
+        if let Value::Sequence(df) = data_flows {
+            for v in df {
+                if let Value::Mapping(m) = v {
+
+                    /*
+                    let id = if let Some(Value::String(value)) = m.get(Value::String(String::from("id"))) {
+                        value.clone()
+                    } else { panic!("No id") };
+                     */
+                    let to = if let Some(Value::String(value)) = m.get(Value::String(String::from("to"))) {
+                        value.clone()
+                    } else { panic!("No to") };
+                    let from = if let Some(Value::String(value)) = m.get(Value::String(String::from("from"))) {
+                        value.clone()
+                    } else { panic!("No from") };
+
+                    // nodes.push(Node::new(id.clone(), id));
+                    edges.push(Edge::new(from, to))
+                }
+            }
+        } else { panic!("Could not parse data flows") }
+
+        let external_entities = &yaml["external entities"];
+        if let Value::Sequence(ee) = external_entities {
+            for v in ee {
+                if let Value::Mapping(m) = v {
+
+                    let id = if let Some(Value::String(value)) = m.get(Value::String(String::from("id"))) {
+                        value.clone()
+                    } else { panic!("No id") };
+
+                    nodes.push(Node::new(id.clone(), id));
+                }
+            }
+        } else { panic!("Could not parse external entities") }
+
+        let processes = &yaml["processes"];
+        if let Value::Sequence(pr) = processes {
+            for v in pr {
+                if let Value::Mapping(m) = v {
+
+                    let id = if let Some(Value::String(value)) = m.get(Value::String(String::from("id"))) {
+                        value.clone()
+                    } else { panic!("No id") };
+
+                    nodes.push(Node::new(id.clone(), id));
+                }
+            }
+        } else { panic!("Could not parse processes") }
+
+        let data_stores = &yaml["data stores"];
+        if let Value::Sequence(ds) = data_stores {
+            for v in ds {
+                if let Value::Mapping(m) = v {
+
+                    let id = if let Some(Value::String(value)) = m.get(Value::String(String::from("id"))) {
+                        value.clone()
+                    } else { panic!("No id") };
+
+                    nodes.push(Node::new(id.clone(), id));
+                }
+            }
+        } else { panic!("Could not parse data stores") }
+
+        Data { nodes, edges }
     }
 }   
 
--- plugins/dfd/src/lib.rs
+++ plugins/dfd/src/lib.rs
@@ -17,8 +17,9 @@ pub fn greet(s: &str) -> String {
 
 #[wasm_bindgen]
 pub fn process(s: &str) -> String {
-    let n1 = Node::new(1, s.into());
-    let n2 = Node::new(2, "a".into());
+    /*
+    let n1 = Node::new(String::from("1"), s.into());
+    let n2 = Node::new(String::from("2"), "a".into());
 
     let e1 = Edge::new(n1.get_id(), n2.get_id());
 
@@ -26,23 +27,10 @@ pub fn process(s: &str) -> String {
     let json_res = serde_json::to_string(&data).unwrap();
 
     json_res
-    /*
-    r#" 
-    {
-        "nodes": [
-            { "id": 1, "label": "Node 1" },
-            { "id": 2, "label": "Node 2" },
-            { "id": 3, "label": "Node 3" },
-            { "id": 4, "label": "Node 4" },
-            { "id": 5, "label": "Node 5" }
-        ],
-        "edges": [
-            { "from": 1, "to": 2 },
-            { "from": 1, "to": 3 },
-            { "from": 2, "to": 4 },
-            { "from": 2, "to": 5 }
-        ]
-    }
-    "#.into()
     */
+    let data = Data::from_yaml(s);
+
+    let json_res = serde_json::to_string(&data).unwrap();
+
+    json_res
 }
